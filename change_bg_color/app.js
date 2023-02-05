7ecd7f51762443460efcddb88034e1adf2ced47a
Rezaul Karim <shuvothesuperman@gmail.com>
2023-02-05 10:37:37 +0600

    add some extra color

--- change_bg_color/app.js
+++ change_bg_color/app.js
@@ -1,6 +1,8 @@
 const btn = document.querySelector('button');
 const body = document.querySelector('body');
-const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
+const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple','white','orange','black'];
+body.style.backgroundColor = 'gray';
 btn.addEventListener('click',function (){
-    console.log('click')
+    let color = parseInt(Math.random() * colors.length);
+    body.style.backgroundColor = colors[color];
 })
