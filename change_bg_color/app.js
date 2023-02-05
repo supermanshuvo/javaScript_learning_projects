d828f606de284c71947ace74fc03fdee81111e25
Rezaul Karim <shuvothesuperman@gmail.com>
2023-02-05 10:31:33 +0600

    add body variable

--- change_bg_color/app.js
+++ change_bg_color/app.js
@@ -1,4 +1,6 @@
 const btn = document.querySelector('button');
+const body = document.querySelector('body');
+const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
 btn.addEventListener('click',function (){
     console.log('click')
 })
